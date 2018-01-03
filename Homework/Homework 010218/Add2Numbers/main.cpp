56f09eca2db356a464168ff785dcd9366e495d59
AntonioGines <agines2@student.rccd.edu>
2018-01-02 19:45:08 -0800

    Signed-off-by: AntonioGines <agines2@student.rccd.edu>

---
+++ Homework/Homework 010218/Add2Numbers/main.cpp
@@ -0,0 +1,36 @@
+/* 
+ * File:   main.cpp
+ * Author: Antonio Gines
+ * Created on January 2, 2018, 1:30 PM
+ *Purpose: Add 2 Numbers
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants - Math/Physics Constants, Conversions, 2-D Array Dimensions
+
+//Function Prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //Declare Variables
+    short a, b, c;//Three integer variables
+    
+    //Initialize Variables
+    a=2;
+    b=3;
+    
+    //Process/Map Inputs to Outputs
+    c=a+b;
+            
+    //Output Data
+    cout<<c<<" = "<<a<<" + "<<b<<endl;
+    
+    //Exit Stage Right
+    return 0;
+}
+
---
+++ Homework/Homework 010218/Cpp_Template/main.cpp
@@ -0,0 +1,30 @@
+/* 
+ * File:   main.cpp
+ * Author: Antonio Gines
+ * Created on January 2, 2018, 12:30 PM
+ *Purpose: CIS/CSC 5 Template
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants - Math/Physics Constants, Conversions, 
+//                     2-D Array Dimensions
+
+//Function Prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //Declare Variables
+    
+    //Process/Map Inputs to Outputs
+    
+    //Output Data
+    
+    //Exit Stage Right;
+    return 0;
+}
+
